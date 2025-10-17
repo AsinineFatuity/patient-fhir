52953be2b2575d545d4d95edfc4113525356d540
kembo <asininefatuity@gmail.com>
2025-10-17 06:15:12 +0300

    hacked the data validation

--- create_patient.py
+++ create_patient.py
@@ -1,6 +1,7 @@
 import json 
-
+from fhir.resources.patient import Patient
 with open('sample_patient.json', 'r') as file:
-    patient_data = json.load(file)
+    patient_data_dict = json.load(file)
 
-print(patient_data)
+validated_patient_data = Patient.model_validate(patient_data_dict)
+print(validated_patient_data)
